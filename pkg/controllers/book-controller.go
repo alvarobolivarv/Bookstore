b14d4c99c2c413b8354fbe54a2987ce38dd42703
bw8 <>
2022-04-28 19:21:07 +0200

    Fixed spacing surrounding :=

--- pkg/controllers/book-controller.go
+++ pkg/controllers/book-controller.go
@@ -13,8 +13,8 @@ import(
 var NewBook models.Book
 
 func GetBook(w http.ResponseWriter, r *http.Request){
-    newBooks:=models.GetAllBooks()
-    res, _ :=json.Marshal(newBooks)
+    newBooks := models.GetAllBooks()
+    res, _ := json.Marshal(newBooks)
     w.Header().Set("Content-Type", "pkglication/json")
     w.WriteHeader(http.StatusOK)
     w.Write(res)
