63b1e90fa96e487a0b90ad217f5585f97b75d6ca
bw8 <>
2022-04-28 19:11:22 +0200

    Typo.

--- pkg/routes/bookstore-routes.go
+++ pkg/routes/bookstore-routes.go
@@ -5,7 +5,7 @@ import (
     "github.com/alvarobolivarv/Bookstore/pkg/controllers"
 )
 
-var RegisterBookStore = func(router *mux.Router){
+var RegisterBookStoreRoutes = func(router *mux.Router){
     router.HandleFunc("/book/", controllers.CreateBook).Methods("POST")
     router.HandleFunc("/book/", controllers.GetBook).Methods("GET")
     router.HandleFunc("/book/{bookId}", controllers.GetBookById).Methods("GET")
